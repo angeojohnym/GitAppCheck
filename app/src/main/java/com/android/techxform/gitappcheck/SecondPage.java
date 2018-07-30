edb2aba1e2ba36d34af81f69e4cebf7d59481ce4
Angeo <angeo.techxform@gmail.com>
2018-07-30 14:57:42 +0530

    Second commit

---
+++ app/src/main/java/com/android/techxform/gitappcheck/SecondPage.java
@@ -0,0 +1,13 @@
+package com.android.techxform.gitappcheck;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+}
